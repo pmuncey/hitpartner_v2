d62122730bd4580400cbc47b0ad6b4ec78c5739e
Raghu Betina <raghu@firstdraft.com>
2017-03-08 22:37:10 +0000

    Whitelist columns in Match for ActiveAdmin

--- app/admin/match.rb
+++ app/admin/match.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Match do
 
+ permit_params :player_id, :hitpartner_id, :court_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
