bbc196617546c171d4afc8f3a270f7188db3837f
Raghu Betina <raghu@firstdraft.com>
2017-03-08 22:36:45 +0000

    Direct association between users and matches

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,9 @@
 class Match < ApplicationRecord
   # Direct associations
 
+  belongs_to :hitpartner,
+             :class_name => "User"
+
   belongs_to :player,
              :class_name => "User"
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :hitpartner_matches,
+             :class_name => "Match",
+             :foreign_key => "hitpartner_id",
+             :dependent => :destroy
+
   has_many   :player_matches,
              :class_name => "Match",
              :foreign_key => "player_id",
